189201f5c935b90258b57316a89d3aeca59c79b9
LeslieAmaya <leslieamayal001@gmail.com>
2023-10-31 10:12:12 -0700

    Repository

---
+++ ProyectoCrud.DAL/Repositorio/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoCrud.Models;
+
+namespace ProyectoCrud.DAL.Repositorio
+{
+    public interface IGenericRepository<T> where T: class
+    {
+        Task<bool> Insertar(T modelo);
+        Task<bool> Actualizar(T modelo);
+        Task<T> Obtener(int id);
+        Task<bool> Eliminar(int id);
+        Task<IQueryable<T>> ObtenerTodo ();
+    }
+}
